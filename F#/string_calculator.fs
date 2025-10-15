8bb70ac93e500e01c0f5fdf37b08ae3e5b8b6468
jorgearayac <araya.cepeda.jorge@gmail.com>
2025-10-15 10:24:33 -0300

    Basic Task 1 Implementation, comments pending

--- F#/string_calculator.fs
+++ F#/string_calculator.fs
@@ -1,1 +1,13 @@
 module EnhancedStringCalculator
+
+let add (input: string) : int =
+    match input with
+    | "" -> 0
+    | _ -> 
+        input.Split("")
+        |> Array.map int
+        |> Array.sum
+
+// Manual testing
+printfn "%d" (add "")          // Output: 0
+printfn "%d" (add "1")         // Output: 1
