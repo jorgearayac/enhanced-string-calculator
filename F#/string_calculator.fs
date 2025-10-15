f4e53b345a7b7bd1efce5a1c76815373126ff88e
jorgearayac <araya.cepeda.jorge@gmail.com>
2025-10-15 12:37:57 -0300

    Test case for Task 4 added

--- F#/string_calculator.fs
+++ F#/string_calculator.fs
@@ -3,8 +3,8 @@ module EnhancedStringCalculator
 // There will be comments in the code to help the understanding 
 // of F# in comparison to Python.
 
-// Task 3: Breaking Newlines
-// Allow the method to handle newline characters as delimiters.
+// Task 4: Custom Delimiters
+// Support custom delimiters defined in the format //[delimiter]\n[numbers...]
 
 let add (input: string) : int =
     match input with
@@ -21,3 +21,4 @@ printfn "%d" (add "1")         // Output: 1
 printfn "%d" (add "1,2")       // Output: 3
 printfn "%d" (add "1,2,3,4")   // Output: 10
 printfn "%d" (add "1\n2,3")    // Output: 6
+printfn "%d" (add "//;\n1;2")  // Output: 3
