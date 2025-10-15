ce2de028f70bc1e7cd5ef00b881766d88c533097
jorgearayac <araya.cepeda.jorge@gmail.com>
2025-10-15 11:05:10 -0300

    Comments comparing Python and F# implementation

--- F#/string_calculator.fs
+++ F#/string_calculator.fs
@@ -5,8 +5,13 @@ module EnhancedStringCalculator
 
 // let add() creates a function in the same way def add() does in Python.
 let add (input: string) : int =
+    // match is F#'s version of `if/else`. It checks what the input is.
     match input with
+    // First, we check if the input is an empty string.
+    // `if input == "":`; `->` as `returns 0` in Python
     | "" -> 0
+    // The underscore `_` is a wildcard that matches anything.
+    // This is similar to `else:` in Python.
     | _ -> 
         input.Split("")
         |> Array.map int
